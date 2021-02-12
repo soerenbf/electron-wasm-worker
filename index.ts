9d1a9806d814025f605064c9e8d1bcebc9377c4e
Søren Bruus Zeppelin <sz@concordium.com>
2021-02-12 13:18:44 +0100

    ..

--- index.ts
+++ index.ts
@@ -4,8 +4,4 @@ import Worker from './worker/test.worker';
 
 const worker = new PromiseWorker(new Worker());
 
-worker.postMessage("test").then(_ => console.log("works??")).catch(e => console.error(e));
-
-// import("./pkg").then(module => {
-//   module.greet("Test");
-// });
+worker.postMessage(null);
