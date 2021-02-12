10e977d82b99dbabc0a1a4a99454b55296cfef6a
Søren Bruus Zeppelin <sz@concordium.com>
2021-02-12 10:21:10 +0100

    Working with webpack 5

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,23 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const WasmPackPlugin = require("@wasm-tool/wasm-pack-plugin");
+const NodePolyfill = require("node-polyfill-webpack-plugin");
 
 module.exports = {
+  mode: "development",
   entry: "./index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js"
   },
+  experiments: {
+    syncWebAssembly: true
+  },
   plugins: [
     new HtmlWebpackPlugin(),
-
     new WasmPackPlugin({
       crateDirectory: path.resolve(__dirname, ".")
     }),
+    new NodePolyfill()
   ]
 };
