55eb47601b1f90d48596c55bd23a30ceb34b3f5c
Søren Bruus Zeppelin <sz@concordium.com>
2021-02-12 10:04:51 +0100

    initial working commit

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WasmPackPlugin = require("@wasm-tool/wasm-pack-plugin");
+
+module.exports = {
+  entry: "./index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js"
+  },
+  plugins: [
+    new HtmlWebpackPlugin(),
+
+    new WasmPackPlugin({
+      crateDirectory: path.resolve(__dirname, ".")
+    }),
+  ]
+};
