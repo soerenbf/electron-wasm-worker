53ca1abaff2e322703eea27e922fd22d197d843f
Søren Bruus Zeppelin <sz@concordium.com>
2021-02-15 08:36:55 +0100

    add wasm outside worker example

--- app/index.ts
+++ app/index.ts
@@ -1,7 +1,13 @@
-import PromiseWorker from 'promise-worker';
-// @ts-expect-error : has no default export.
-import Worker from './worker/test.worker';
+// import PromiseWorker from 'promise-worker';
+// // @ts-expect-error : has no default export.
+// import Worker from './worker/test.worker';
 
-const worker = new PromiseWorker(new Worker());
+// const worker = new PromiseWorker(new Worker());
 
-worker.postMessage("").then(console.log);
+// worker.postMessage("").then(console.log);
+
+(async () => {
+    const rust = await import("../pkg");
+    const greeting = rust.get_greeting("Test");
+    console.log(greeting);
+})();
